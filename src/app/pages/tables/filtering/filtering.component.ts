a4055118af4fda052991348a8e3be177485cce87
Juan Carlos Soto <carotos@hotmail.com>
2018-04-21 16:31:00 -0600

    data tables with filtering and paging

--- src/app/pages/tables/filtering/filtering.component.ts
+++ src/app/pages/tables/filtering/filtering.component.ts
@@ -1,24 +1,29 @@
-import { Component } from '@angular/core';
-import { MatTableDataSource } from '@angular/material';
+import {Component, ViewChild} from '@angular/core';
+import {MatPaginator, MatTableDataSource} from '@angular/material';
 import { AppSettings } from '../../../app.settings';
 import { Settings } from '../../../app.settings.model';
 import { TablesService, Element } from '../tables.service';
 
 @Component({
-  selector: 'app-filtering',
-  templateUrl: './filtering.component.html'
+    selector: 'app-filtering',
+    templateUrl: './filtering.component.html'
 })
 export class FilteringComponent {
-  public displayedColumns = ['position', 'name', 'weight', 'symbol'];
-  public dataSource: any;
-  public settings: Settings;
-  constructor(public appSettings:AppSettings, private tablesService:TablesService) {
-    this.settings = this.appSettings.settings; 
-    this.dataSource = new MatTableDataSource<Element>(this.tablesService.getData());
-  }
+    @ViewChild(MatPaginator) paginator: MatPaginator;
+    public displayedColumns = ['messages', 'edit', 'hot', 'axisref', 'pocustomerref', 'sovendorref', 'requesteddate', 'shippingdate', 'eta', 'delivery', 'shipper', 'consignee', 'commentslog', 'carrier', 'service', 'status', 'hu', 'wt', 'quoteno', 'pickupno', 'prono', 'duration'];
+    public dataSource: any;
+    public settings: Settings;
+    constructor(public appSettings: AppSettings, private tablesService: TablesService) {
+        this.settings = this.appSettings.settings;
+        this.dataSource = new MatTableDataSource<Element>(this.tablesService.getData());
+    }
 
-  applyFilter(filterValue: string) {
-    this.dataSource.filter = filterValue.trim().toLowerCase();
-  }
+    applyFilter(filterValue: string) {
+        this.dataSource.filter = filterValue.trim().toLowerCase();
+    }
+
+    ngAfterViewInit() {
+        this.dataSource.paginator = this.paginator;
+    }
 
 }
