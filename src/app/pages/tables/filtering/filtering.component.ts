0a0515fb3c35af5725f2764ae2a2f0a9cd738fb1
diego-torres <dtorresf@redhat.com>
2018-04-21 21:08:05 -0500

    show order comments on icon click

--- src/app/pages/tables/filtering/filtering.component.ts
+++ src/app/pages/tables/filtering/filtering.component.ts
@@ -3,17 +3,19 @@ import {MatPaginator, MatTableDataSource} from '@angular/material';
 import { AppSettings } from '../../../app.settings';
 import { Settings } from '../../../app.settings.model';
 import { TablesService, Element } from '../tables.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-filtering',
-    templateUrl: './filtering.component.html'
+    templateUrl: './filtering.component.html',
+    styleUrls:['./filtering.component.scss']
 })
 export class FilteringComponent {
     @ViewChild(MatPaginator) paginator: MatPaginator;
     public displayedColumns = ['messages', 'edit', 'hot', 'axisref', 'pocustomerref', 'sovendorref', 'requesteddate', 'shippingdate', 'eta', 'delivery', 'shipper', 'consignee', 'commentslog', 'carrier', 'service', 'status', 'hu', 'wt', 'quoteno', 'pickupno', 'prono', 'duration'];
     public dataSource: any;
     public settings: Settings;
-    constructor(public appSettings: AppSettings, private tablesService: TablesService) {
+    constructor(public appSettings: AppSettings, private tablesService: TablesService, private router: Router) {
         this.settings = this.appSettings.settings;
         this.dataSource = new MatTableDataSource<Element>(this.tablesService.getData());
     }
@@ -24,6 +26,10 @@ export class FilteringComponent {
 
     ngAfterViewInit() {
         this.dataSource.paginator = this.paginator;
+    }
+
+    displayComments(){
+        this.router.navigate(['/chat']);
     }
 
 }
