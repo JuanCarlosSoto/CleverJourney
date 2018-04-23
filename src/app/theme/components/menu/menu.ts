58603f69acd9c63b852cf60b76f5a62b3e172c25
diego-torres <dtorresf@redhat.com>
2018-04-22 20:21:27 -0500

    menus for orders

--- src/app/theme/components/menu/menu.ts
+++ src/app/theme/components/menu/menu.ts
@@ -4,11 +4,13 @@ import { Menu } from './menu.model';
 export const verticalMenuItems = [ 
     new Menu (1, 'Dashboard', '/', null, 'dashboard', null, false, 0),
     new Menu (2, 'Users', '/users', null, 'supervisor_account', null, false, 0), 
-    new Menu (3, 'Orders', '/tables/filtering', null, 'inbox', null, false, 0),
+    new Menu (3, 'Upload Orders', '/form-controls/form-field', null, 'note_add', null, false, 0),
+    new Menu (4, 'Existent Orders', '/tables/filtering', null, 'inbox', null, false, 0),
 ]
 
 export const horizontalMenuItems = [ 
     new Menu (1, 'Dashboard', '/', null, 'dashboard', null, false, 0),
     new Menu (2, 'Users', '/users', null, 'supervisor_account', null, false, 0), 
-    new Menu (3, 'Orders', '/tables/filtering', null, 'inbox', null, false, 0),
+    new Menu (3, 'Upload Orders', '/form-controls/form-field', null, 'note_add', null, false, 0),
+    new Menu (4, 'Existent Orders', '/tables/filtering', null, 'inbox', null, false, 0),
 ]
