ad0be405b6b3d5eeb47e97f395a3a0f2528e4065
diego-torres <dtorresf@redhat.com>
2018-04-22 21:50:39 -0500

    add and edit actions in grid

--- src/app/pages/tables/filtering/filtering.component.ts
+++ src/app/pages/tables/filtering/filtering.component.ts
@@ -1,5 +1,5 @@
-import {Component, ViewChild} from '@angular/core';
-import {MatPaginator, MatTableDataSource} from '@angular/material';
+import { Component, ViewChild } from '@angular/core';
+import { MatPaginator, MatTableDataSource, MatSort } from '@angular/material';
 import { AppSettings } from '../../../app.settings';
 import { Settings } from '../../../app.settings.model';
 import { TablesService, Element } from '../tables.service';
@@ -8,10 +8,11 @@ import { Router } from '@angular/router';
 @Component({
     selector: 'app-filtering',
     templateUrl: './filtering.component.html',
-    styleUrls:['./filtering.component.scss']
+    styleUrls: ['./filtering.component.scss']
 })
 export class FilteringComponent {
     @ViewChild(MatPaginator) paginator: MatPaginator;
+    @ViewChild(MatSort) sort: MatSort;
     public displayedColumns = ['messages', 'edit', 'hot', 'axisref', 'pocustomerref', 'sovendorref', 'requesteddate', 'shippingdate', 'eta', 'delivery', 'shipper', 'consignee', 'commentslog', 'carrier', 'service', 'status', 'hu', 'wt', 'quoteno', 'pickupno', 'prono', 'duration'];
     public dataSource: any;
     public settings: Settings;
@@ -26,10 +27,19 @@ export class FilteringComponent {
 
     ngAfterViewInit() {
         this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
     }
 
-    displayComments(){
+    displayComments() {
         this.router.navigate(['/chat']);
+    }
+
+    displayAdd() {
+        this.router.navigate(['./form-controls/form-field']);
+    }
+
+    displayEdit() {
+        this.router.navigate(['./form-controls/form-field']);
     }
 
 }
