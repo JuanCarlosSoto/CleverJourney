6e514108b84e6e5c66646305184caae52d9110d1
diego-torres <dtorresf@redhat.com>
2018-04-27 15:07:56 -0500

    #12 fix dashboard aging and amount order lists to don't break the edit form action

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -21,7 +21,8 @@ export class DashboardComponent implements OnInit {
     public circle: any;
     public map: any;
 
-    public displayedColumns = ['edit', 'pocustomerref', 'requesteddate', 'loc'];
+    //public displayedColumns = ['edit', 'pocustomerref', 'requesteddate', 'loc'];
+    public displayedColumns = ['pocustomerref', 'requesteddate', 'loc'];
 
     constructor(public appSettings: AppSettings, private tablesService: TablesService, private router: Router) {
         this.settings = this.appSettings.settings;
